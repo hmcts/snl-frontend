5b13d55aaefd83e6c08799ff6a4fef0f384e5ecc
Remigiusz Kruk <R.Kruk@kainos.com>
2018-04-19 12:39:49 +0200

    change nginx config path

--- docker/run_nginx.sh
+++ docker/run_nginx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export DOLLAR='$'
-envsubst < config.json.template > /usr/share/nginx/html/config.json
+envsubst < config.json.template > /usr/share/nginx/html/browser/config.json
 nginx -g "daemon off;"
