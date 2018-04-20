06e6d89ea30b0f226fce24ed8f0f56536d989d9d
radoslawlandowskikainos <r.landowski@kainos.com>
2018-04-20 13:02:21 +0200

    Try cors

--- server.ts
+++ server.ts
@@ -37,6 +37,12 @@ app.engine('html', ngExpressEngine({
   ]
 }));
 
+app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+});
+
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
