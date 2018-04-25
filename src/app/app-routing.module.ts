ffe3e6d29f4dc05e4de0cc86d0204ae740ca2488
radoslawlandowskikainos <r.landowski@kainos.com>
2018-04-25 11:03:05 +0200

    add dummy create session component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
         path: 'sessions',
         component: SessionsPageComponent,
         children: [
+            { path: '', redirectTo: 'search', pathMatch: 'full' },
             {
                 path: 'search',
                 component: SessionsSearchComponent
