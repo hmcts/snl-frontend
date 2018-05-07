75241fcf2900ddfed49fac8c41a18081e3f2d8fd
radoslawlandowskikainos <r.landowski@kainos.com>
2018-05-07 16:05:18 +0200

    Check session lenght

--- src/app/sessions/containers/session-table/session-table.component.ts
+++ src/app/sessions/containers/session-table/session-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { MatTableDataSource } from '@angular/material';
 import * as fromSessions from '../../reducers/session.reducer';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-session-table',
@@ -12,12 +13,17 @@ export class SessionTableComponent implements OnInit {
 
   displayedColumns = ['position', 'time', 'duration'];
   dataSource;
+  tableVisible;
 
   constructor(private store: Store<fromSessions.State>) {
+    this.tableVisible = false;
 
     this.store.pipe(select(fromSessions.getSessionsEntities)).subscribe(data => {
+      this.tableVisible = data.length !== 0;
       if (data && data.map) {
-        data.map(element => { element.start = new Date(element.start); });
+        data.map(element => {
+          element.start = new Date(element.start);
+        });
         this.dataSource = new MatTableDataSource(data);
       }
     });
