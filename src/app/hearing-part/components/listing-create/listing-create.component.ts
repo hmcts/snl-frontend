dcbb345af9c91fa6ecb00be82c4e5456992ffb89
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-05-09 17:52:05 +0200

    SL-898 Basic required fields check

--- src/app/hearing-part/components/listing-create/listing-create.component.ts
+++ src/app/hearing-part/components/listing-create/listing-create.component.ts
@@ -3,6 +3,9 @@ import { Store } from '@ngrx/store';
 import { State } from '../../../app.state';
 import { ListingCreate } from '../../models/listing-create';
 import * as moment from 'moment';
+import { v4 as uuid } from 'uuid';
+
+const DURATION_UNIT = 'minute';
 
 @Component({
     selector: 'app-listing-create',
@@ -12,33 +15,72 @@ import * as moment from 'moment';
 export class ListingCreateComponent implements OnInit {
     hearings: string[];
     caseTypes: string[];
+    duration = 0;
+    errors = '';
 
     listing: ListingCreate;
 
     constructor(private store: Store<State>) {
         this.hearings = ['Preliminary Hearing', 'Trial Hearing', 'Adjourned Hearing'];
         this.caseTypes = ['SCLAIMS', 'FTRACK', 'MTRACK'];
-
-        this.listing = {
-            id: undefined,
-            caseNumber: undefined,
-            caseType: undefined,
-            hearingType: undefined,
-            duration: moment.duration(0),
-            scheduleStart: undefined,
-            scheduleEnd: undefined,
-        } as ListingCreate;
+        this.initiateListing();
     }
 
     create() {
-        // let time_arr = this.time.split(':');
-        // this.listing.start.setHours(time_arr[0]);
-        // this.listing.start.setMinutes(time_arr[1]);
-        // this.listing.id = uuid();
-        // this.store.dispatch(new SessionActions.Create(this.listing));
+        this.listing.id = uuid();
+        this.listing.duration.add(this.duration, DURATION_UNIT);
+        if (this.checkRequiredFields(this.listing)) {
+            if ((this.listing.scheduleStart === null && this.listing.scheduleEnd !== null)
+                || (this.listing.scheduleStart !== null && this.listing.scheduleEnd === null)
+                || (this.listing.scheduleStart !== null
+                    && this.listing.scheduleEnd !== null
+                    && this.listing.scheduleStart.getDate() > this.listing.scheduleEnd.getDate())
+            ) {
+                this.errors = 'Start date should be before End date';
+            } else {
+                // this.store.dispatch(new Action())
+
+                this.initiateListing();
+            }
+        } else {
+            this.errors = 'Required (*) field is missing value';
+        }
     }
 
     ngOnInit() {
     }
 
+    private initiateListing() {
+        this.listing = {
+            id: undefined,
+            caseNumber: undefined,
+            caseTitle: undefined,
+            caseType: undefined,
+            hearingType: undefined,
+            duration: moment.duration(0, DURATION_UNIT),
+            scheduleStart: null,
+            scheduleEnd: null,
+        } as ListingCreate;
+        this.duration = 0;
+        this.errors = '';
+    }
+
+    private checkRequiredFields(listing: ListingCreate) {
+        if (listing.caseNumber === undefined || listing.caseNumber.trim() === '') {
+            return false;
+        }
+        if (listing.caseTitle === undefined || listing.caseTitle.trim() === '') {
+            return false;
+        }
+        if (listing.caseType === undefined || listing.caseType.trim() === '') {
+            return false;
+        }
+        if (listing.hearingType === undefined || listing.hearingType.trim() === '') {
+            return false;
+        }
+        if (listing.duration === undefined || listing.duration.get(DURATION_UNIT) === 0) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/app/hearing-part/models/listing-create.ts
+++ src/app/hearing-part/models/listing-create.ts
@@ -3,6 +3,7 @@ import * as moment from 'moment'
 export interface ListingCreate {
     id: string;
     caseNumber: string;
+    caseTitle: string;
     caseType: string;
     hearingType: string
     duration: moment.Duration
