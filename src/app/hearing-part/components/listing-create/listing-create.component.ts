e2ddf510f6984fff854c97373095b78a00d28b87
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-05-09 09:55:44 +0200

    SL-898 initial view skeleton

---
+++ src/app/hearing-part/components/listing-create/listing-create.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { SessionCreate } from '../../../sessions/models/session-create.model';
+import * as JudgeActions from '../../../judges/actions/judge.action';
+import * as RoomActions from '../../../rooms/actions/room.action';
+import { Store } from '@ngrx/store';
+import * as SessionActions from '../../../sessions/actions/session.action';
+import { State } from '../../../app.state';
+import { v4 as uuid } from 'uuid';
+import { ListingCreate } from '../../models/listing-create';
+import * as moment from 'moment'
+
+@Component({
+    selector: 'app-listing-create',
+    templateUrl: './listing-create.component.html',
+    styleUrls: ['./listing-create.component.scss']
+})
+export class ListingCreateComponent implements OnInit {
+    hearings: string[];
+    caseTypes: string[];
+    time;
+
+    listing: ListingCreate;
+
+    constructor(private store: Store<State>) {
+        this.hearings = ['Preliminary Hearing', 'Trial Hearing', 'Adjourned Hearing'];
+        this.caseTypes = ['SCLAIMS', 'FTRACK', 'MTRACK'];
+
+        this.listing = {
+            id: undefined,
+            caseNumber: undefined,
+            caseType: undefined,
+            hearingType: undefined,
+            duration: moment.duration(0),
+            scheduleStart: undefined,
+            scheduleEnd: undefined,
+        } as ListingCreate;
+    }
+
+    create() {
+        // let time_arr = this.time.split(':');
+        // this.listing.start.setHours(time_arr[0]);
+        // this.listing.start.setMinutes(time_arr[1]);
+        // this.listing.id = uuid();
+        // this.store.dispatch(new SessionActions.Create(this.listing));
+    }
+
+    ngOnInit() {
+    }
+
+}
