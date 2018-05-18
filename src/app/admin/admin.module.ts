c804970089de4625c2a9a7301ea22a28019385ef
radoslawlandowskikainos <r.landowski@kainos.com>
2018-05-18 13:45:22 +0200

    Remove redundant code from admin module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,48 +1,17 @@
-import { Injectable, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecurityModule } from '../security/security.module';
 import { PocService } from './services/poc-service';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
-import {
-    HTTP_INTERCEPTORS,
-    HttpClientXsrfModule,
-    HttpEvent,
-    HttpHandler,
-    HttpInterceptor,
-    HttpRequest,
-    HttpXsrfTokenExtractor
-} from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-
-@Injectable()
-export class HttpXsrfInterceptor implements HttpInterceptor {
-
-    constructor(private tokenExtractor: HttpXsrfTokenExtractor) {
-    }
-
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const headerName = 'X-XSRF-TOKEN';
-        let token = this.tokenExtractor.getToken() as string;
-        if (token !== null && !req.headers.has(headerName)) {
-            req = req.clone({headers: req.headers.set(headerName, token)});
-        }
-        return next.handle(req);
-    }
-}
 
 @NgModule({
     imports: [
         CommonModule,
         SecurityModule,
-        AngularMaterialModule,
-        HttpClientXsrfModule.withOptions({
-            cookieName: 'XSRF-TOKEN', // this is optional
-            headerName: 'X-XSRF-TOKEN' // this is optional
-        }),
+        AngularMaterialModule
     ],
   providers: [
-    PocService,
-      {provide: HTTP_INTERCEPTORS, useClass: HttpXsrfInterceptor, multi: true},
+    PocService
   ]
 })
 export class AdminModule {
