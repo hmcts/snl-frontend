5d17fad3d507d76e3bc6d1132b4049e2fb2355b7
Kamil Zielinski <K.Zielinski@kainos.com>
2018-06-12 15:44:56 +0200

    Added References description to table, updated scss

--- src/app/problems/components/problems-table/problems-table.component.ts
+++ src/app/problems/components/problems-table/problems-table.component.ts
@@ -1,3 +1,4 @@
+import { ProblemReference } from './../../models/problem-reference.model';
 import { Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { Problem } from '../../models/problem.model';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
@@ -8,22 +9,27 @@ import { MatPaginator, MatTableDataSource } from '@angular/material';
   styleUrls: ['./problems-table.component.scss']
 })
 export class ProblemsTableComponent implements OnInit, OnChanges {
-  displayedColumns = ['id', 'message'];
+  displayedColumns = ['id', 'message', 'references description'];
   dataSource;
 
-  @Input()
-  problems: Problem[];
+  @Input() problems: Problem[];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngOnChanges() {
-      this.dataSource = new MatTableDataSource(Object.values(this.problems));
-      this.dataSource.paginator = this.paginator;
+    this.dataSource = new MatTableDataSource(Object.values(this.problems));
+    this.dataSource.paginator = this.paginator;
   }
 
+  extractRefDescriptions(element: Problem): string {
+    const descriptionReferences = element.references
+      .map((reference: ProblemReference) => `<li>${reference.entity}: ${reference.description}</li>`)
+      .join(' ');
+
+    return `<ul>${descriptionReferences}</ul>`;
+  }
 }
