80bb704f30e62db0e7204a7f44ac0a586e06e42c
radoslawlandowskikainos <r.landowski@kainos.com>
2018-06-19 16:04:14 +0200

    Remove problems before obtaining new ones

--- src/app/sessions/services/sessions-creation.service.ts
+++ src/app/sessions/services/sessions-creation.service.ts
@@ -3,11 +3,10 @@ import { SessionCreate } from '../models/session-create.model';
 import * as SessionCreationActions from '../actions/session-transaction.action';
 import { SessionTransaction } from '../models/session-transaction-status.model';
 import * as SessionActions from '../actions/session.action';
+import * as ProblemsActions from '../../problems/actions/problem.action';
 import { Store } from '@ngrx/store';
 import { State } from '../../app.state';
 import { v4 as uuid } from 'uuid';
-import { Session } from '../models/session.model';
-import { InitializeTransaction } from '../actions/session-transaction.action';
 
 @Injectable()
 export class SessionsCreationService {
@@ -16,37 +15,30 @@ export class SessionsCreationService {
 
     create(session: SessionCreate) {
         let transactionId = uuid();
-        let transaction = {
-            entityId: session.id,
-            id: transactionId
-        } as SessionTransaction;
 
         session.userTransactionId = transactionId;
+        let transaction = this.createTransaction(session.id, transactionId);
 
         this.store.dispatch(new SessionCreationActions.InitializeTransaction(transaction));
         this.store.dispatch(new SessionActions.Create(session));
+        this.store.dispatch(new ProblemsActions.RemoveAll());
     }
 
     update(session: any) {
         let transactionId = uuid();
-        let transaction = {
-            entityId: session.id,
-            id: transactionId
-        } as SessionTransaction;
 
         session.userTransactionId = transactionId;
+        let transaction = this.createTransaction(session.id, transactionId);
 
         this.store.dispatch(new SessionCreationActions.InitializeTransaction(transaction));
         this.store.dispatch(new SessionActions.Update(session));
+        this.store.dispatch(new ProblemsActions.RemoveAll());
     }
 
-    retrieveSessionFromEvent(event: any): Session {
+    private createTransaction(sessionId, transactionId): SessionTransaction {
         return {
-            id: event.id,
-            start: event.start,
-            duration: event.duration,
-            room: event.room,
-
-        } as Session;
+            entityId: sessionId,
+            id: transactionId
+        } as SessionTransaction;
     }
 }
