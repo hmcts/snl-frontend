b025f0647fa76d336179f3eb396b44efaf33a404
radoslawlandowskikainos <r.landowski@kainos.com>
2018-06-26 11:46:27 +0200

    Remove fake method

--- src/app/features/reports/services/report-service.ts
+++ src/app/features/reports/services/report-service.ts
@@ -10,33 +10,7 @@ export class ReportService {
     }
 
     getUnlistedHearingRequests(): Observable<UnlistedHearingRequest[]> {
-        //return this.fake();
         return this.http
             .get<UnlistedHearingRequest[]>(`${this.config.getApiUrl()}/report/unlisted-hearing-requests`)
-    }
-
-    fake(): Observable<UnlistedHearingRequest[]> {
-        return Observable.of([
-            {
-                'minutes': 360,
-                'title': 'Require listing in the next 4 weeks',
-                'hearings': 6
-            },
-            {
-                'minutes': 360,
-                'title': 'Require listing in the next 3 months',
-                'hearings': 6
-            },
-            {
-                'minutes': 45,
-                'title': 'Require listing beyond the next 3 months',
-                'hearings': 1
-            },
-            {
-                'minutes': 0,
-                'title': 'Target schedule not provided',
-                'hearings': 0
-            }
-        ])
     }
 }
