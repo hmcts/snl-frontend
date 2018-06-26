d0d1b67cbca12a04adf961a51f785e4303406ac8
radoslawlandowskikainos <r.landowski@kainos.com>
2018-06-26 09:27:56 +0200

    Remove useless code

--- src/app/features/reports/containers/reports-container.component.ts
+++ src/app/features/reports/containers/reports-container.component.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { UnlistedHearingRequest } from '../model/unlisted-hearings/unlisted-hearing-request';
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-reports-container',
@@ -12,7 +10,6 @@ export class ReportsContainerComponent {
 
     chosenReport: string;
     reportNames: string[];
-    data: Observable<UnlistedHearingRequest[]>;
 
     constructor() {
         this.reportNames = ['Unlisted hearings'];
