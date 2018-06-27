55d03d54c5ca6cb075ebb12880e2b8d34e92b70f
Kamil Zielinski <K.Zielinski@kainos.com>
2018-06-27 11:21:55 +0200

    set default dev login & password

--- e2e/configs/credentials.ts
+++ e2e/configs/credentials.ts
@@ -1,6 +1,6 @@
 export class Credentials {
-    static ValidUsername = process.env.SNL_UITEST_VALID_USERNAME;
-    static ValidPassword = process.env.SNL_UITEST_VALID_PASSWORD;
+    static ValidUsername = 'officer1';
+    static ValidPassword = 'asd';
     static InvalidUsername = 'invalidUsername';
     static InvalidPassword = 'invalidPassword';
 }
