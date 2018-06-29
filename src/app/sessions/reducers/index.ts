c8c5026bdf35dbd8514e8855682cce62d841e141
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-06-29 10:19:50 +0200

    SL-975 Optimised Session finding selector

--- src/app/sessions/reducers/index.ts
+++ src/app/sessions/reducers/index.ts
@@ -10,6 +10,7 @@ import { Session } from '../models/session.model';
 import { SessionProposition } from '../models/session-proposition.model';
 import { SessionPropositionView } from '../models/session-proposition-view.model';
 import * as moment from 'moment';
+import { Dictionary } from '@ngrx/entity/src/models';
 
 export interface SessionsState {
     readonly sessions: fromSessions.State;
@@ -137,8 +138,8 @@ export const getSessionViewModelById = (id: string) => createSelector(getFullSes
     return svm.find(s => s.id === id);
 });
 
-export const getSessionById = (id: string) => createSelector(getAllSessions, (sessions: Session[]) => {
-    return sessions.find(s => s.id === id);
+export const getSessionById = (id: string) => createSelector(getSessionEntities, (sessions: Dictionary<Session>) => {
+    return sessions[id];
 });
 
 export const getRecentlyCreatedSession = createSelector(
