18e16bb923cbf82d8c4bf4ba2e658a3eb5a76367
Kamil Zielinski <K.Zielinski@kainos.com>
2018-07-20 10:01:23 +0200

    changed resolution in headless chrome

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -14,7 +14,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: isHeadlessModeEnabled ? ['--headless', '--no-sandbox', '--disable-dev-shm-usage'] : [],
+      args: isHeadlessModeEnabled ? ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--window-size=1920,1080'] : [],
       binary: puppeteer.executablePath(),
     }
   },
