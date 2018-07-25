25237c55db17819abccbedf60df51ffa752f6558
Remigiusz Kruk <R.Kruk@kainos.com>
2018-07-25 11:56:07 +0200

    change smoke test

--- smoke-test/basic.smoke-spec.ts
+++ smoke-test/basic.smoke-spec.ts
@@ -1,10 +1,14 @@
 import { browser } from 'protractor';
+import { LoginPage } from '../e2e/pages/login.po';
+
+const loginPage = new LoginPage();
 
 describe('SNL frontend smoke tests', () => {
     describe('health check', () => {
         it('should get page', () => {
             browser.get('/');
 
+            loginPage.isDisplayed();
             expect(browser.getTitle()).toEqual('Scheduling and Listing');
         });
     });
