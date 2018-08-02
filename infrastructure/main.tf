237ef37c280a38233279cfe2e9a3f5d5a9100849
radoslawlandowskikainos <r.landowski@kainos.com>
2018-08-02 09:23:05 +0200

    SL-1105: set terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,13 @@
 locals {
   app_full_name = "${var.product}-${var.component}"
+
+  aat_api_url = "http://snl-api-aat.service.core-compute-aat.internal"
+  local_api_url = "http://snl-api-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+  api_url = "${var.env == "preview" ? local.aat_api_url : local.local_api_url}"
+
+  aat_notes_url = "http://snl-notes-aat.service.core-compute-aat.internal"
+  local_notes_url = "http://snl-notes-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+  notes_url = "${var.env == "preview" ? local.aat_notes_url : local.local_notes_url}"
 }
 module "snl-frontend" {
   source               = "git@github.com:hmcts/moj-module-webapp"
@@ -17,6 +25,8 @@ module "snl-frontend" {
     # REDIS_PORT                   = "${module.redis-cache.redis_port}"
     # REDIS_PASSWORD               = "${module.redis-cache.access_key}"
     # RECIPE_BACKEND_URL = "http://snl-recipe-backend-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
-    SNL_API_URL = "http://snl-api-aat.service.core-compute-aat.internal"
+
+    SNL_API_URL = "${local.api_url}"
+    SNL_NOTES_URL = "${local.notes_url}"
   }
 }
