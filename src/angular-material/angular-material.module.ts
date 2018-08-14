3f335230068e273f709b82d6cd73e3875a4f9833
Szymon Filipczyk <s.filipczyk@kainos.com>
2018-08-14 16:44:00 +0200

    fix dateFormat parsing

--- src/angular-material/angular-material.module.ts
+++ src/angular-material/angular-material.module.ts
@@ -26,7 +26,7 @@ import {
     MAT_DATE_FORMATS,
     MatSortModule
 } from '@angular/material';
-import { MomentDateAdapter } from '../../node_modules/@angular/material-moment-adapter';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '../../node_modules/@angular/material-moment-adapter';
 import { AppConfig } from '../app/app.config';
 
 @NgModule({
@@ -82,12 +82,14 @@ import { AppConfig } from '../app/app.config';
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: AppConfig.locale},
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: {
-            display: {
-                monthYearLabel: 'MMM YYYY',
-            },
+    {provide: MAT_DATE_FORMATS, useValue: {...MAT_MOMENT_DATE_FORMATS,
+        display: {
+          monthYearLabel: 'MMM YYYY',
+        },
+        parse: {
+          dateInput: 'DD/MM/YYYY'
         }
-    },
+    }},
   ]
 })
 export class AngularMaterialModule { }
