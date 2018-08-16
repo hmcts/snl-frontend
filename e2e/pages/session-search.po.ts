1eab6a48f58612e486f5149faf64c76a3da5af86
Remigiusz Kruk <R.Kruk@kainos.com>
2018-08-16 13:26:29 +0200

    add changing max items per table page

--- e2e/pages/session-search.po.ts
+++ e2e/pages/session-search.po.ts
@@ -7,25 +7,32 @@ import { FilterSessionComponent } from '../components/filter-session';
 import { Table } from '../components/table';
 
 export class SessionSearchPage {
-  private filterSessionComponent = new FilterSessionComponent();
-  private sessionsTable = new Table(element(by.id('sessions-table')))
-  private listingRequestsTable = new Table(element(by.css('mat-table#hearing-part-preview')))
-  public assignButton = element(by.id('assign'));
+    private filterSessionComponent = new FilterSessionComponent();
+    private sessionsTable = new Table(element(by.id('sessions-table')));
+    private listingRequestsTable = new Table(element(by.css('mat-table#hearing-part-preview')));
+    public assignButton = element(by.id('assign'));
 
-  filterSession(formValues: FilterSessionsComponentForm) {
-    this.filterSessionComponent.filter(formValues);
-  }
+    filterSession(formValues: FilterSessionsComponentForm) {
+        this.filterSessionComponent.filter(formValues);
+    }
 
-  async selectSession(judge: Judges, date: string, time: string, room: Rooms, caseType: CaseTypes) {
-    this.selectCheckBoxInRowWithValues(this.sessionsTable, judge, date, time, room, caseType)
-  }
+    async selectSession(judge: Judges, date: string, time: string, room: Rooms, caseType: CaseTypes) {
+        this.selectCheckBoxInRowWithValues(this.sessionsTable, judge, date, time, room, caseType)
+    }
 
-  async selectListingRequest(caseNumber: string, caseTitle: string, caseType: CaseTypes,
-    targetScheduleFrom: string, targetScheduleTo: string) {
-      this.selectCheckBoxInRowWithValues(this.listingRequestsTable, caseNumber, caseTitle, caseType, targetScheduleFrom, targetScheduleTo)
-  }
+    changeMaxItemsPerPage(value: string) {
+        element.all(by.css('.mat-paginator-page-size')).each(el => {
+            el.element(by.css('mat-select')).click();
+            element(by.cssContainingText('.mat-option-text', value)).click();
+        });
+    }
 
-  private selectCheckBoxInRowWithValues(table: Table, ...values: string[]) {
-    table.rowThatContains(...values).element(by.css('mat-checkbox')).click()
-  }
+    async selectListingRequest(caseNumber: string, caseTitle: string, caseType: CaseTypes,
+        targetScheduleFrom: string, targetScheduleTo: string) {
+        this.selectCheckBoxInRowWithValues(this.listingRequestsTable, caseNumber, caseTitle, caseType, targetScheduleFrom, targetScheduleTo)
+    }
+
+    private selectCheckBoxInRowWithValues(table: Table, ...values: string[]) {
+        table.rowThatContains(...values).element(by.css('mat-checkbox')).click()
+    }
 }
