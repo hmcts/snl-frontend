3bbe057e2b908128687814fc6ac96bbc5bbdc385
Kamil Zielinski <K.Zielinski@kainos.com>
2018-08-29 11:20:01 +0200

    SL-1110: refactor login flow

--- e2e/specs/login.e2e-spec.ts
+++ e2e/specs/login.e2e-spec.ts
@@ -1,15 +1,15 @@
+import { LoginFlow } from './../flows/login.flow';
 import { browser } from 'protractor';
 import { LoginPage } from '../pages/login.po';
 import { Credentials } from '../enums/credentials';
 
-let loginPage: LoginPage
+const loginPage = new LoginPage()
+const loginFlow = new LoginFlow()
 
 describe('Login', () => {
-  beforeEach(() => {
-    loginPage = new LoginPage();
-  });
   describe('When login using invalid credentials', () => {
-    it('should not change URL', () => {
+    it('should not change URL', async () => {
+      await loginFlow.logoutIfNeeded()
       const expectedURL = browser.getCurrentUrl()
       loginPage.login('invalidUserName', 'invalidPassword')
       expect(browser.getCurrentUrl()).toEqual(expectedURL)
@@ -18,8 +18,8 @@ describe('Login', () => {
   describe('When login using valid credentials', () => {
     it('should change URL to calendar', () => {
       loginPage.login(Credentials.ValidOfficerUsername, Credentials.ValidOfficerPassword);
-        browser.waitForAngular();
-        expect(browser.getCurrentUrl()).toContain('/home/calendar');
+      browser.waitForAngular();
+      expect(browser.getCurrentUrl()).toContain('/home/calendar');
     });
   });
 });
