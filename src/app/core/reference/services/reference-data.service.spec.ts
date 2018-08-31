a37d7e567e026c7e45f1e11009cf0bb05c0b700b
radoslawlandowskikainos <r.landowski@kainos.com>
2018-08-31 12:46:15 +0200

    SL-1237: ref data service spec

---
+++ src/app/core/reference/services/reference-data.service.spec.ts
@@ -0,0 +1,71 @@
+import { TestBed } from '@angular/core/testing';
+import {
+    HttpClientTestingModule,
+    HttpTestingController
+} from '@angular/common/http/testing';
+import { AppConfig } from '../../../app.config';
+import { ReferenceDataService } from './reference-data.service';
+
+const mockedAppConfig = { getApiUrl: () => 'https://google.co.uk' };
+
+let httpMock: HttpTestingController;
+let referenceDataService: ReferenceDataService;
+
+describe('ReferenceDataService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                ReferenceDataService,
+                { provide: AppConfig, useValue: mockedAppConfig }
+            ]
+        });
+        referenceDataService = TestBed.get(ReferenceDataService);
+        httpMock = TestBed.get(HttpTestingController);
+    });
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    fdescribe('getCaseTypes', () => {
+        const expectedUrl = `${mockedAppConfig.getApiUrl()}/reference/case-types`;
+
+        it('should call proper url', () => {
+            referenceDataService.getCaseTypes().subscribe(
+                data => expect(data).toEqual([])
+            );
+            httpMock.expectOne(expectedUrl).flush([]);
+        });
+    });
+    fdescribe('getHearingTypes', () => {
+        const expectedUrl = `${mockedAppConfig.getApiUrl()}/reference/hearing-types`;
+
+        it('should call proper url', () => {
+            referenceDataService.getHearingTypes().subscribe(
+                data => expect(data).toEqual([])
+            );
+            httpMock.expectOne(expectedUrl).flush([]);
+        });
+    });
+    fdescribe('getSessionTypes', () => {
+        const expectedUrl = `${mockedAppConfig.getApiUrl()}/reference/session-types`;
+
+        it('should call proper url', () => {
+            referenceDataService.getSessionTypes().subscribe(
+                data => expect(data).toEqual([])
+            );
+            httpMock.expectOne(expectedUrl).flush([]);
+        });
+    });
+    fdescribe('getRoomTypes', () => {
+        const expectedUrl = `${mockedAppConfig.getApiUrl()}/reference/room-types`;
+
+        it('should call proper url', () => {
+            referenceDataService.getRoomTypes().subscribe(
+                data => expect(data).toEqual([])
+            );
+            httpMock.expectOne(expectedUrl).flush([]);
+        });
+    });
+
+});
