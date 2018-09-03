e4f8a60c1b45fef2faed92c0aba0b29d0c084a2f
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-03 12:25:58 +0200

    added more params to try fix chrome connection timeout

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    flags: [
+      '--disable-web-security',
+      '--disable-gpu',
+      '--no-sandbox'
+    ],
     singleRun: false
   });
 };
