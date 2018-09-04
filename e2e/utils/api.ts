996b16dd797d93ccdf782d54575bcd4b6c3f0b6c
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-04 08:16:31 +0200

    SL-1111: changed base URL for e2e tests

--- e2e/utils/api.ts
+++ e2e/utils/api.ts
@@ -1,16 +1,16 @@
 import { Credentials } from '../enums/credentials';
 import { CreateListingRequestBody } from '../models/create-listing-request-body';
-import { CONFIG } from '../../url-config';
 import * as rm from 'typed-rest-client/HttpClient';
 
 export class API {
+    private static baseUrl = (process.env.TEST_URL || 'http://localhost:3451/').replace('https', 'http');
     private static headers = { 'Authorization': '', 'Content-Type': 'application/json' }
     private static rest = new rm.HttpClient('e2e-tests', null, {headers: API.headers});
 
     static async createListingRequest(body: CreateListingRequestBody): Promise<number> {
         await API.login();
-        const response = await this.rest.put(`${CONFIG.apiUrl}/hearing-part/create`, JSON.stringify(body), API.headers);
-        await this.rest.post(`${CONFIG.apiUrl}/user-transaction/${body.userTransactionId}/commit`, JSON.stringify(body), API.headers);
+        const response = await this.rest.put(`${API.baseUrl}/hearing-part/create`, JSON.stringify(body), API.headers);
+        await this.rest.post(`${API.baseUrl}/user-transaction/${body.userTransactionId}/commit`, JSON.stringify(body), API.headers);
         return response.message.statusCode;
     }
 
@@ -19,7 +19,7 @@ export class API {
             username: Credentials.ValidOfficerUsername,
             password: Credentials.ValidOfficerPassword
         };
-        const response = await API.rest.post(`${CONFIG.apiUrl}/security/signin`, JSON.stringify(body), API.headers)
+        const response = await API.rest.post(`${API.baseUrl}/security/signin`, JSON.stringify(body), API.headers)
         const responseBody = JSON.parse(await response.readBody());
         API.headers.Authorization = `${responseBody.tokenType} ${responseBody.accessToken}`;
     }
