a01c953390991f1461b815191faf5bb3c4d49d74
Kamil Pakur <k.pakur@kainos.com>
2018-09-17 16:43:20 +0200

    SL-1737 fix for multipage scraling for e2e

--- e2e/pages/session-search.po.ts
+++ e2e/pages/session-search.po.ts
@@ -8,11 +8,14 @@ import { Table } from '../components/table';
 import { Wait } from '../enums/wait';
 import { SessionTypes } from '../enums/session-types';
 import { HearingTypes } from '../enums/hearing-types';
+import { Paginator } from '../components/paginator';
 
 export class SessionSearchPage {
     private filterSessionComponent = new FilterSessionComponent();
     private sessionsTable = new Table(element(by.id('sessions-table')));
-    private listingRequestsTable = new Table(element(by.css('app-hearing-parts-preview#hearing-part-preview')));
+    private sessionsTablePaginator = new Paginator(element(by.id('sessions-table-paginator')));
+    private listingRequestsTable = new Table(element(by.id('hearings-part-table')));
+    private listingRequestsTablePaginator = new Paginator(element(by.id('hearings-part-table-paginator')));
     public assignButton = element(by.id('assign'));
 
     async clickAssignButton() {
@@ -25,7 +28,8 @@ export class SessionSearchPage {
     }
 
     async selectSession(judge: Judges, date: string, time: string, room: Rooms, sessionType?: SessionTypes) {
-        await this.selectCheckBoxInRowWithValues(this.sessionsTable, judge, date, time, room, sessionType)
+        await this.selectCheckBoxInRowWithValues(this.sessionsTable, this.sessionsTablePaginator,
+          judge, date, time, room, sessionType)
     }
 
     async changeMaxItemsPerPage(value: string): Promise<any> {
@@ -53,18 +57,19 @@ export class SessionSearchPage {
 
     async selectListingRequest(caseNumber: string, caseTitle: string, caseType: CaseTypes, hearingType: HearingTypes,
         targetScheduleFrom: string, targetScheduleTo: string) {
-        await this.selectCheckBoxInRowWithValues(this.listingRequestsTable,
+        await this.selectCheckBoxInRowWithValues(this.listingRequestsTable, this.listingRequestsTablePaginator,
             caseNumber, caseTitle, caseType, hearingType, targetScheduleFrom, targetScheduleTo)
     }
 
     async isListingRequestDisplayed(...values: string[]): Promise<boolean> {
-        const row = await this.listingRequestsTable.rowThatContains(...values)
-        await browser.wait(ExpectedConditions.visibilityOf(row), Wait.normal, `Listing request with values: ${values} is not visible`)
-        return await row.isDisplayed()
+      let row = await this.listingRequestsTable.rowThatContainsAtAnyPage(this.listingRequestsTablePaginator, ...values);
+      await browser.wait(ExpectedConditions.visibilityOf(row), Wait.normal, `Listing request with values: ${values} is not visible`)
+
+      return await row.isDisplayed()
     }
 
     async editListingRequestWithValues(...values: string[]): Promise<void> {
-        const row = await this.listingRequestsTable.rowThatContains(...values)
+        const row = await this.listingRequestsTable.rowThatContainsAtAnyPage(this.listingRequestsTablePaginator, ...values)
         await row.element(by.cssContainingText('.clickable', 'edit')).click()
     }
 
@@ -72,8 +77,8 @@ export class SessionSearchPage {
         await browser.wait(ExpectedConditions.visibilityOf(this.assignButton), Wait.normal, 'Session search page is not visible')
     }
 
-    private async selectCheckBoxInRowWithValues(table: Table, ...values: string[]) {
-        const row = await table.rowThatContains(...values)
+    private async selectCheckBoxInRowWithValues(table: Table, paginator: Paginator, ...values: string[]) {
+        let row = await table.rowThatContainsAtAnyPage(paginator, ...values)
         await row.element(by.css('mat-checkbox')).click()
     }
 }
