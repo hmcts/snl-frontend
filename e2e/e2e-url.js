5c172b59c58b2b6152b87017685419fde2f32d42
Kamil Zielinski <K.Zielinski@kainos.com>
2018-09-20 12:14:19 +0200

    added e2e-url.js to store URLs

---
+++ e2e/e2e-url.js
@@ -0,0 +1,13 @@
+var attEnv = {
+    proxy: 'http://proxyout.reform.hmcts.net:8080',
+    frontendURL: "http://snl-frontend-aat.service.core-compute-aat.internal",
+    apiURL: "http://snl-api-aat.service.core-compute-aat.internal"
+};
+
+var localEnv = {
+    frontendURL: "http://localhost:3451/",
+    apiURL: "http://localhost:8090"
+};
+
+// module.exports = attEnv;
+module.exports = localEnv;
