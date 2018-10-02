9282999b12e5f16aa18fa6bf1f012e53ac43872d
Szymon Filipczyk <s.filipczyk@kainos.com>
2018-10-02 13:31:38 +0200

    SL-1768: remove usuned GOV.UK and HMCTS modules

--- src/app/hmcts/hmcts.module.ts
+++ src/app/hmcts/hmcts.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HmctsGlobalHeaderComponent } from './components/hmcts-global-header/hmcts-global-header.component';
-import { HmctsProgressBarComponent } from './components/hmcts-progress-bar/hmcts-progress-bar.component';
 import { HmctsPrimaryNavigationComponent } from './components/hmcts-primary-navigation/hmcts-primary-navigation.component';
 import { HmctsSubNavigationComponent } from './components/hmcts-sub-navigation/hmcts-sub-navigation.component';
-import { HmctsTimelineComponent } from './components/hmcts-timeline/hmcts-timeline.component';
-import {RouterModule} from '@angular/router';
-import {SentenceCasePipe} from './pipes/sentence-case/sentence-case-pipe';
-import { HmctsAlertComponent } from './components/hmcts-alert/hmcts-alert.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
@@ -16,21 +12,13 @@ import { HmctsAlertComponent } from './components/hmcts-alert/hmcts-alert.compon
     ],
     declarations: [
         HmctsGlobalHeaderComponent,
-        HmctsProgressBarComponent,
         HmctsPrimaryNavigationComponent,
-        HmctsSubNavigationComponent,
-        HmctsTimelineComponent,
-        HmctsAlertComponent,
-        SentenceCasePipe
+        HmctsSubNavigationComponent
     ],
     exports: [
         HmctsGlobalHeaderComponent,
-        HmctsProgressBarComponent,
         HmctsPrimaryNavigationComponent,
         HmctsSubNavigationComponent,
-        HmctsTimelineComponent,
-        HmctsAlertComponent,
-        SentenceCasePipe
     ]
 })
 export class HmctsModule {
