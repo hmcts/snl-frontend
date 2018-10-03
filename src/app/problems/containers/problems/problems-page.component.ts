5e17373830c8e5321f96581165aba72f9b7ed42d
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-03 15:23:04 +0200

    SL-1564: changed approach. Bypass store. extended problems service

--- src/app/problems/containers/problems/problems-page.component.ts
+++ src/app/problems/containers/problems/problems-page.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Problem, isGreater, Severity } from '../../models/problem.model';
-import * as fromProblems from '../../reducers';
-import * as fromProblemsPartsActions from '../../actions/problem.action'
-import { Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
+import { Problem } from '../../models/problem.model';
+import { ProblemsService } from '../../services/problems.service';
 
 @Component({
   selector: 'app-problems-page',
@@ -11,44 +8,11 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./problems-page.component.scss']
 })
 export class ProblemsPageComponent implements OnInit {
-
     problems: Problem[] = [];
 
-    constructor(private readonly store: Store<fromProblems.State>) {
-        this.store.select(fromProblems.getProblems).pipe(
-            map(data => data ? Object.values(data) : []),
-            map(this.sortByCreatedAtDescending),
-            map(this.sortBySeverity)
-        ).subscribe(problems => this.problems = problems)
-    }
+    constructor(private readonly problemsService: ProblemsService) { }
 
     ngOnInit() {
-        this.store.dispatch(new fromProblemsPartsActions.Get())
-    }
-
-    sortBySeverity(problems: Problem[]): Problem[] {
-        return problems.sort((a, b) => {
-            const aSeverity = Severity[a.severity] || Severity.Warning
-            const bSeverity = Severity[b.severity] || Severity.Warning
-            if (aSeverity === bSeverity) {
-                return 0
-            } else if (isGreater(aSeverity, bSeverity)) {
-                return -1;
-            } else {
-                return 1;
-            }
-        });
-    }
-
-    sortByCreatedAtDescending(problems: Problem[]): Problem[] {
-        return problems.sort((a, b) => {
-            if (!a.createdAt.isValid()) {
-                return 1;
-            } else if (!b.createdAt.isValid()) {
-                return -1;
-            } else {
-                return b.createdAt.diff(a.createdAt)
-            }
-        });
+        this.problemsService.getAll().subscribe(problems => this.problems = problems);
     }
 }
--- src/app/problems/models/problem.model.ts
+++ src/app/problems/models/problem.model.ts
@@ -1,6 +1,14 @@
 import { ProblemReference } from './problem-reference.model';
 import * as moment from 'moment';
 
+export interface ProblemResponse {
+    id: string;
+    message: string;
+    severity: string;
+    type: string;
+    references: ProblemReference[];
+    createdAt: string
+}
 export interface Problem {
     id: string;
     message: string;
