042ff16ddc40989ea3591f40b154a9e0a398b02f
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-03 11:25:46 +0200

    SL-1564: added unit tests for sorting

--- src/app/problems/containers/problems/problems-page.component.spec.ts
+++ src/app/problems/containers/problems/problems-page.component.spec.ts
@@ -6,53 +6,11 @@ import { reducers } from '../../reducers';
 import * as problemsActions from '../../actions/problem.action';
 import * as fromProblems from '../../reducers';
 import * as moment from 'moment';
+import { v4 as uuid } from 'uuid';
 
 let problemsPageComponent: ProblemsPageComponent;
 let storeSpy: jasmine.Spy;
 let store: Store<fromProblems.State>;
-
-const theNewestProblem: Problem = {
-    id: '1',
-    message: undefined,
-    severity: undefined,
-    type: undefined,
-    references: undefined,
-    createdAt: moment()
-}
-
-const theOldestProblem: Problem = {
-    id: '3',
-    message: undefined,
-    severity: undefined,
-    type: undefined,
-    references: undefined,
-    createdAt: moment().subtract(2, 'hours')
-}
-
-const middleProblem: Problem = {
-    id: '2',
-    message: undefined,
-    severity: undefined,
-    type: undefined,
-    references: undefined,
-    createdAt: moment().subtract(1, 'hours')
-}
-
-const problemWithUndefinedCreatedAt: Problem = {
-    id: '4',
-    message: undefined,
-    severity: undefined,
-    type: undefined,
-    references: undefined,
-    createdAt: moment(null)
-}
-
-const problems: Problem[] = [
-    problemWithUndefinedCreatedAt,
-    theNewestProblem,
-    theOldestProblem,
-    middleProblem
-];
 
 describe('ProblemsPageComponent', () => {
     beforeEach(() => {
@@ -80,18 +38,51 @@ describe('ProblemsPageComponent', () => {
         expect(passedObj instanceof problemsActions.Get).toBeTruthy();
     });
 
-    it('should populate problems', () => {
+    it('should set sorted problems by priority and date', () => {
+        const theNewestWarningProblem = problemGenerator(moment(), 'Warning')
+        const olderUrgentProblem = problemGenerator(moment().subtract(1, 'hours'), 'Urgent')
+        const evenOlderCriticalProblem = problemGenerator(moment().subtract(2, 'hours'), 'Critical')
+        const nowUndefinedSeverityProblem = problemGenerator(moment(null), 'Some Severity')
+        const undefinedCreatedAtCriticalProblem = problemGenerator(moment(null), 'Critical')
+
+        const problems: Problem[] = [
+            theNewestWarningProblem,
+            olderUrgentProblem,
+            evenOlderCriticalProblem,
+            nowUndefinedSeverityProblem,
+            undefinedCreatedAtCriticalProblem
+        ];
         store.dispatch(new problemsActions.GetComplete(problems));
 
-        problemsPageComponent.problems$.subscribe(data => {
-            expect(data.length).toEqual(problems.length);
-            problems.forEach(problem => expect(data).toContain(problem))
-        })
+        problemsPageComponent = TestBed.get(ProblemsPageComponent);
+        problemsPageComponent.ngOnInit();
+
+        const expectedProblemOrder: Problem[] = [
+            evenOlderCriticalProblem,
+            undefinedCreatedAtCriticalProblem,
+            olderUrgentProblem,
+            theNewestWarningProblem,
+            nowUndefinedSeverityProblem
+        ]
+
+        expect(problemsPageComponent.problems).toEqual(expectedProblemOrder)
     });
 
     describe('sortByCreatedAtDescending', () => {
         it('should sort by created at property - the newest problems should be first', () => {
+            const theNewestProblem = problemGenerator(moment())
+            const problemWithUndefinedCreatedAt = problemGenerator(moment(null))
+            const theOldestProblem = problemGenerator(moment().subtract(2, 'hours'))
+            const middleProblem = problemGenerator(moment().subtract(1, 'hours'))
+            const problems: Problem[] = [
+                problemWithUndefinedCreatedAt,
+                theNewestProblem,
+                theOldestProblem,
+                middleProblem,
+            ];
+
             const sortedProblems = problemsPageComponent.sortByCreatedAtDescending(problems)
+
             expect(sortedProblems).toEqual([
                 theNewestProblem,
                 middleProblem,
@@ -100,4 +91,39 @@ describe('ProblemsPageComponent', () => {
             ])
         });
     });
+
+    describe('sortBySeverity', () => {
+        it('should sort by severity - Critical first, Warning last', () => {
+            const criticalProblem = problemGenerator(moment(), 'Critical')
+            const urgentProblem = problemGenerator(moment(), 'Urgent')
+            const warningProblem = problemGenerator(moment(), 'Warning')
+            const undefinedSeverityProblem = problemGenerator(moment(), 'SomeString')
+
+            const problems: Problem[] = [
+                urgentProblem,
+                warningProblem,
+                undefinedSeverityProblem,
+                criticalProblem
+            ]
+
+            const sortedProblems = problemsPageComponent.sortBySeverity(problems)
+            expect(sortedProblems).toEqual([
+                criticalProblem,
+                urgentProblem,
+                warningProblem,
+                undefinedSeverityProblem
+            ])
+        });
+    });
 });
+
+function problemGenerator(createdAt: moment.Moment, severity = 'Warning'): Problem {
+    return {
+        id: uuid(),
+        message: undefined,
+        severity: severity,
+        type: undefined,
+        references: undefined,
+        createdAt: createdAt
+    }
+}
