63cf1cede32fb0ea85efd56249718bfc4243c3a6
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-03 11:23:52 +0200

    SL-1564: added severity enum + isGreater function

--- src/app/problems/models/problem.model.ts
+++ src/app/problems/models/problem.model.ts
@@ -9,3 +9,19 @@ export interface Problem {
     references: ProblemReference[];
     createdAt: moment.Moment
 }
+
+export enum Severity {
+    Warning = 'Warning',
+    Urgent = 'Urgent',
+    Critical = 'Critical'
+}
+
+export function isGreater(lhs: Severity, rhs: Severity) {
+    if (lhs === Severity.Critical && (rhs === Severity.Urgent || rhs === Severity.Warning)) {
+        return 1;
+    } else if (lhs === Severity.Urgent && (rhs === Severity.Warning)) {
+        return 1;
+    } else {
+        return 0
+    }
+}
