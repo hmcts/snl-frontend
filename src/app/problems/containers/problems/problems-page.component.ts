af777b8a70d799fe502bf06447bf8ce93e3cfc05
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-05 11:55:27 +0200

    refactored problems page component, use changed problem service + adjust it to child

--- src/app/problems/containers/problems/problems-page.component.ts
+++ src/app/problems/containers/problems/problems-page.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Problem } from '../../models/problem.model';
+import { Component } from '@angular/core';
+import { Problem, Page } from '../../models/problem.model';
 import { ProblemsService } from '../../services/problems.service';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-problems-page',
   templateUrl: './problems-page.component.html',
   styleUrls: ['./problems-page.component.scss']
 })
-export class ProblemsPageComponent implements OnInit {
+export class ProblemsPageComponent {
     problems: Problem[] = [];
+    totalCount = 0
 
     constructor(private readonly problemsService: ProblemsService) { }
 
-    ngOnInit() {
-        this.problemsService.getAll().subscribe(problems => this.problems = problems);
+    fetchProblems(pageEvent: PageEvent) {
+        this.problemsService
+            .getAll(pageEvent.pageSize, pageEvent.pageIndex)
+            .subscribe((pagedProblems: Page<Problem>) => {
+                this.problems = pagedProblems.content;
+                this.totalCount = pagedProblems.totalElements
+            })
     }
 }
