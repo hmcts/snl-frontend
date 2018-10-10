1ec904f7f94b5509e2de2edc13e2ce8230f418e0
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-10 14:34:09 +0200

    SL-1627: fixed issue with e2e test to wait for all events to load

--- e2e/pages/calendar.po.ts
+++ e2e/pages/calendar.po.ts
@@ -31,7 +31,12 @@ export class CalendarPage {
   }
 
   async clickOnEventWith(startTime: string) {
-    await element.all(by.cssContainingText('.fc-list-item-time', startTime)).last().click()
+    const eventSelektor = by.cssContainingText('.fc-list-item-time', startTime);
+    await browser.wait(
+      ExpectedConditions.presenceOf(element.all(eventSelektor).last()),
+      Wait.normal,
+      `Event with start time: '${startTime}' is not present`);
+    await element.all(eventSelektor).last().click()
     const dialog = element(by.css('mat-dialog-container'))
     await browser.wait(ExpectedConditions.visibilityOf(dialog), Wait.normal, `Event with start time: ${startTime} haven't appear`)
   }
