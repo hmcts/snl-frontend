a956249fca49e0af14d36a1276cea3c031a91ae2
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-16 11:08:07 +0200

    SL-1599: extracted NotesConfiguration common part to separate base class

---
+++ src/app/notes/services/note-configuration.ts
@@ -0,0 +1,28 @@
+import { NoteViewmodel } from '../models/note.viewmodel';
+import { NoteType } from '../models/note-type';
+
+export class NoteConfiguration {
+    public getOrCreateNote(notes: NoteViewmodel[], ofType: NoteType) {
+        const note = notes.find(n => n.type === ofType)
+        return note || this.noteViewModelOf(ofType)
+    }
+
+    public noteViewModelOf(noteType: NoteType) {
+        return this.noteViewModel(noteType)
+    }
+
+    private noteViewModel(type: string, inputLabel: string = undefined): NoteViewmodel {
+        inputLabel = inputLabel || type;
+        return {
+            id: undefined,
+            type,
+            content: undefined,
+            entityId: undefined,
+            entityType: undefined,
+            createdAt: undefined,
+            modifiedBy: undefined,
+            modified: false,
+            inputLabel: inputLabel,
+        }
+    }
+}
--- src/app/sessions/models/session-create-notes-configuration.model.ts
+++ src/app/sessions/models/session-create-notes-configuration.model.ts
@@ -1,24 +1,7 @@
-import { Note } from '../../notes/models/note.model';
-import { NotesConfiguration } from '../../notes/models/notes-configuration';
 import { Injectable } from '@angular/core';
+import { NoteConfiguration } from '../../notes/services/note-configuration';
 
 @Injectable()
-export class SessionCreateNotesConfiguration implements NotesConfiguration {
-    public readonly entityName;
-
-    constructor() {
-        this.entityName = 'Session';
-    }
-
-    public getNewFreeTextNote(): Note {
-        return {
-            id: undefined,
-            content: '',
-            type: 'Other note'
-        } as Note;
-    }
-
-    public defaultNotes(): Note[] {
-        return [this.getNewFreeTextNote()];
-    }
+export class SessionCreateNotesConfiguration extends NoteConfiguration {
+    public readonly entityName = 'Session';
 }
