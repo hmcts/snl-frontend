9b8405c09f87e3e8c1e25e117c69f164868715b4
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-16 11:10:52 +0200

    SL-1599: updated note component. Added logic to mark as modified only when content has changed

--- src/app/notes/components/note/note.component.ts
+++ src/app/notes/components/note/note.component.ts
@@ -10,17 +10,27 @@ import { FormControl } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NoteComponent implements OnInit {
-    @Input() note: NoteViewmodel;
-    @Input() disabled: boolean;
+    originalContent = null;
+    private _note: NoteViewmodel;
+    @Input() set note(note: NoteViewmodel) {
+        this._note = note;
+        this.originalContent = note.content;
+    }
+    get note(): NoteViewmodel { return this._note };
+
+    @Input() disabled = false;
 
     content: FormControl;
 
     ngOnInit(): void {
-        this.content  = new FormControl({value: '', disabled: this.disabled});
+        this.content = new FormControl({value: '', disabled: this.disabled});
         this.content.valueChanges
-            .subscribe( (value) => {
+            .subscribe((value) => {
                 this.note.modified = true;
                 this.note.content = value;
+                if (this.originalContent === value) {
+                    this.note.modified = false;
+                }
             });
     }
 }
