6310d753bef083cd0ab00b029026cc3dd9295adc
Remigiusz Kruk <R.Kruk@kainos.com>
2018-10-19 11:41:20 +0200

    add test for assign hearing dialog

--- src/app/hearing-part/components/assign-hearing-dialog/assign-hearing-dialog.component.spec.ts
+++ src/app/hearing-part/components/assign-hearing-dialog/assign-hearing-dialog.component.spec.ts
@@ -1,20 +1,23 @@
 import { NotesPreparerService } from '../../../notes/services/notes-preparer.service';
 import { AssignHearingDialogComponent } from './assign-hearing-dialog.component';
 import { HearingAssignmentNotesConfiguration } from '../../models/hearing-assignment-notes-configuration.model';
+import SpyObj = jasmine.SpyObj;
 
 describe('AssignHearingDialogComponent', () => {
   let component: AssignHearingDialogComponent;
   let notesConfiguration: HearingAssignmentNotesConfiguration;
-  let notesPreparerService: NotesPreparerService;
+  let notesPreparerService: SpyObj<NotesPreparerService>;
   let selectedHearingId: string;
   let spyDialogRef: any;
+  const noteList = jasmine.createSpyObj('noteList', ['getModifiedNotes']);
 
   beforeEach(() => {
     spyDialogRef = jasmine.createSpyObj('dialogRef', ['close']);
     notesConfiguration = new HearingAssignmentNotesConfiguration();
-    notesPreparerService = new NotesPreparerService();
+    notesPreparerService = jasmine.createSpyObj('notesPreparerService', ['removeEmptyNotes', 'prepare']);
     selectedHearingId = 'id';
     component = new AssignHearingDialogComponent(spyDialogRef, selectedHearingId, notesConfiguration, notesPreparerService);
+    component.noteList = noteList;
   });
 
   it('should create', () => {
@@ -27,5 +30,29 @@ describe('AssignHearingDialogComponent', () => {
 
       expect(spyDialogRef.close).toHaveBeenCalledWith({ confirmed: false })
     })
-  })
+  });
+
+  describe('Init state', () => {
+    it('form group should be set properly', () => {
+      component.ngOnInit();
+
+      expect(component.startTime).toBeTruthy();
+      expect(component.formGroup).toBeTruthy();
+    })
+  });
+
+  describe('Init state', () => {
+      it('form group should be set properly', () => {
+          noteList.getModifiedNotes.and.returnValue([]);
+          notesPreparerService.prepare.and.returnValue([]);
+          notesPreparerService.removeEmptyNotes.and.returnValue([]);
+          component.onListHearing();
+
+          expect(spyDialogRef.close).toHaveBeenCalledWith({
+              confirmed: true,
+              startTime: this.startTime,
+              notes: []
+          });
+      })
+  });
 });
