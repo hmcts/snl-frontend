f1e36b85d67eb5a69f31aa47cea76879beb12136
Kamil Zielinski <K.Zielinski@kainos.com>
2018-10-26 13:30:27 +0200

    SL-1761: added two dialogs to view-hearing component to handle unlist

--- src/app/hearing/components/view-hearing/view-hearing.component.ts
+++ src/app/hearing/components/view-hearing/view-hearing.component.ts
@@ -1,8 +1,12 @@
+import { DEFAULT_DIALOG_CONFIG } from './../../../features/transactions/models/default-dialog-confg';
 import { Component, OnInit } from '@angular/core';
 import { HearingService } from '../../services/hearing.service';
 import { Hearing, Session } from '../../models/hearing';
 import * as moment from 'moment';
 import { ActivatedRoute } from '@angular/router';
+import { DialogWithActionsComponent } from '../../../features/notification/components/dialog-with-actions/dialog-with-actions.component';
+import { MatDialog } from '@angular/material';
+import { TransactionDialogComponent } from '../../../features/transactions/components/transaction-dialog/transaction-dialog.component';
 
 @Component({
   selector: 'app-view-hearing',
@@ -14,15 +18,13 @@ export class ViewHearingComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private readonly hearingService: HearingService
+    private readonly hearingService: HearingService,
+    private readonly dialog: MatDialog,
   ) {
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id')
-    this.hearingService.getById(id).subscribe(h => {
-      this.hearing = h;
-    });
+    this.fetchHearing();
   }
 
   formatDate(date: string): string {
@@ -60,5 +62,43 @@ export class ViewHearingComponent implements OnInit {
 
   isSessionPanelDisabled(session: Session) {
     return session.notes === undefined || session.notes.length === 0;
+  }
+
+  isListed() {
+    return this.hearing.sessions.length > 0
+  }
+
+  openConfirmationDialog() {
+    const confirmationDialogRef = this.dialog.open(DialogWithActionsComponent, {
+        ...DEFAULT_DIALOG_CONFIG,
+        data: {
+            message: 'Are you sure you want to unlist this hearing?'
+        },
+    });
+
+    confirmationDialogRef.afterClosed().subscribe(this.confirmationDialogClosed);
+  }
+
+  public confirmationDialogClosed = (confirmed: boolean) => {
+    if (confirmed) {
+      this.hearingService.unlist(this.hearing.id, this.hearing.version).subscribe();
+      this.openSummaryDialog().afterClosed().subscribe((success) => {
+          if (success) {
+              this.fetchHearing();
+          }
+      });
+    }
+  };
+
+  private openSummaryDialog() {
+    return this.dialog.open(TransactionDialogComponent, {
+        ...DEFAULT_DIALOG_CONFIG,
+        data: 'Unlist hearing parts from session'
+    });
+  }
+
+  private fetchHearing() {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.hearingService.getById(id).subscribe(h => this.hearing = h);
   }
 }
