331e5533a4dffd49631c9888643e1ec7a69eb98d
radoslawlandowskikainos <r.landowski@kainos.com>
2018-11-13 08:50:34 +0100

    SL-1136: Make type safe base resolver

--- src/app/core/resolvers/base.resolver.ts
+++ src/app/core/resolvers/base.resolver.ts
@@ -7,12 +7,12 @@ export class BaseResolver {
 
     constructor() {}
 
-    getOrFetchData(service: any, dataFetchingFunctionName: string, dataGettingFunctionName: string) {
-        return service[dataGettingFunctionName]().pipe(mergeMap(data => {
+    getOrFetchData(dataFetchingFunction: () => Observable<any[]>, dataGettingFunction: () => Observable<any[]>): Observable<any[]> {
+        return dataGettingFunction().pipe(mergeMap(data => {
             if ((data as any[]).length !== 0) {
                 return Observable.of(data);
             } else {
-                return service[dataFetchingFunctionName]();
+                return dataFetchingFunction();
             }
         }), take(1))
     }
