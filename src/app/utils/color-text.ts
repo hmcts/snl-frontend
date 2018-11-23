da8b009340cf2ecca3a5e178890e8937d160f07e
Kamil Pakur <k.pakur@kainos.com>
2018-11-23 16:07:50 +0100

    SL-1945: tslint fix

--- src/app/utils/color-text.ts
+++ src/app/utils/color-text.ts
@@ -3,11 +3,11 @@ import * as djb2 from 'djb2';
 export function textToColor(text) {
     let hash = djb2(text);
 
-    // noinspection TsLint
+    // tslint:disable-next-line
     let g = (hash & 0xFF0000) >> 16;
-    // noinspection TsLint
+    // tslint:disable-next-line
     let b = (hash & 0x00FF00) >> 8;
-    // noinspection TsLint
+    // tslint:disable-next-line
     let r = hash & 0x0000FF;
 
     return '#' + ('0' + r.toString(16)).substr(-2) + ('0' + g.toString(16)).substr(-2) + ('0' + b.toString(16)).substr(-2);
