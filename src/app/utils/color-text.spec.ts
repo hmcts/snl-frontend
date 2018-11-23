830d23a9b23e514316e0c49030341eb3766ca5ae
Kamil Pakur <k.pakur@kainos.com>
2018-11-23 16:15:06 +0100

    SL-1945: removed test focus

--- src/app/utils/color-text.spec.ts
+++ src/app/utils/color-text.spec.ts
@@ -1,6 +1,6 @@
 import { textToColor } from './color-text';
 
-fdescribe('asArray', () => {
+describe('asArray', () => {
   it('should calculate different colors for different text', () => {
     const color1 = textToColor('this is 1');
     const color2 = textToColor('this is different one');
