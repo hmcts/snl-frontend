a0e6321abe8d5400fda5e029ed9976d8d1577cb6
radoslawlandowskikainos <r.landowski@kainos.com>
2018-11-30 08:26:57 +0100

    SL-1971: fix showing multisession

--- src/app/hearing-part/components/hearing-amend/hearing-amend.component.ts
+++ src/app/hearing-part/components/hearing-amend/hearing-amend.component.ts
@@ -40,6 +40,8 @@ export class HearingAmendComponent {
         }
 
         this.setFormGroup();
+
+        this.limitSessionsMaxValue = this.listing.hearing.numberOfSessions;
     }
 
     @Output() onSave = new EventEmitter();
@@ -73,6 +75,7 @@ export class HearingAmendComponent {
     public numberOfSeconds = 60;
     public binIntMaxValue = 86399; // 24h * 60m * 60s -1s
     public limitMaxValue = this.binIntMaxValue / this.numberOfSeconds;
+    public limitSessionsMaxValue: number;
 
     constructor(private readonly asDaysPipe: DurationAsDaysPipe) {}
 
@@ -162,7 +165,10 @@ export class HearingAmendComponent {
                     [Validators.required, Validators.min(1), Validators.max(this.limitMaxValue)]
                 ),
                 numberOfSessions: new FormControl(
-                    this.listing.hearing.numberOfSessions, [Validators.min(1)]
+                    this.listing.hearing.numberOfSessions, [
+                        Validators.min(this.listing.hearing.multiSession ? 2 : 1),
+                        Validators.max(this.listing.hearing.status === Status.Listed ? this.limitSessionsMaxValue : undefined)
+                    ]
                 )
             }),
             targetDates: new FormGroup({
