ac8fa035332d62f611a6f27f9ca4a6246003b437
Remigiusz Kruk <R.Kruk@kainos.com>
2018-12-07 12:30:49 +0100

    SL-1855: change activityLog service entity mapping

--- src/app/features/activityLog/models/activity-log.model.ts
+++ src/app/features/activityLog/models/activity-log.model.ts
@@ -6,3 +6,19 @@ export interface ActivityLog {
     createdBy: string,
     createdAt: moment.Moment;
 }
+
+export interface ActivityLogResponse {
+    activityStatus: string,
+    description: string,
+    createdBy: string,
+    createdAt: string;
+}
+
+export function mapResponseToActivityLog(activityLogResponse: ActivityLogResponse): ActivityLog {
+    return {
+        activityStatus: activityLogResponse.activityStatus,
+        description: activityLogResponse. description,
+        createdBy: activityLogResponse.createdBy,
+        createdAt: moment(activityLogResponse.createdAt)
+    }
+}
--- src/app/features/activityLog/services/activity-log.service.ts
+++ src/app/features/activityLog/services/activity-log.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AppConfig } from '../../../app.config';
 import { Observable } from 'rxjs/Observable';
-import { ActivityLog } from '../models/activity-log.model';
+import { ActivityLog, ActivityLogResponse, mapResponseToActivityLog } from '../models/activity-log.model';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ActivityLogService {
@@ -11,7 +12,10 @@ export class ActivityLogService {
 
     getActivitiesForEntity(entityId: string): Observable<ActivityLog[]> {
         return this.http
-            .get<ActivityLog[]>(`${this.getUrl()}/${entityId}`)
+            .get<ActivityLogResponse[]>(`${this.getUrl()}/${entityId}`)
+            .pipe(map<ActivityLogResponse[], ActivityLog[]>(activityLogs => {
+                return activityLogs.map(mapResponseToActivityLog)
+            }));
     }
 
     private getUrl() {
