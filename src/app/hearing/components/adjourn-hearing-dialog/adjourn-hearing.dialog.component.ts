159844a646bef88d24f497362df9e7647f6da121
Remigiusz Kruk <R.Kruk@kainos.com>
2018-12-14 10:19:45 +0100

    SL-1878: change adjournment form

--- src/app/hearing/components/adjourn-hearing-dialog/adjourn-hearing.dialog.component.ts
+++ src/app/hearing/components/adjourn-hearing-dialog/adjourn-hearing.dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { DialogWithActionsComponent } from '../../../features/notification/components/dialog-with-actions/dialog-with-actions.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-dialog-actions',
@@ -9,13 +10,21 @@ import { DialogWithActionsComponent } from '../../../features/notification/compo
 })
 export class AdjournHearingDialogComponent extends DialogWithActionsComponent {
     description: string;
+    adjournmentReasonFormGroup: FormGroup;
 
     constructor(public dialogRef: MatDialogRef<any>, @Inject(MAT_DIALOG_DATA) public data: any) {
         super(dialogRef, data);
+        this.initiateFormGroup();
     }
 
     onOkClick(): void {
         this.data.description = this.description;
         super.onOkClick();
+    }
+
+    private initiateFormGroup() {
+        this.adjournmentReasonFormGroup = new FormGroup({
+            description: new FormControl(this.description, [Validators.required, Validators.maxLength(500)])
+        })
     }
 }
